25ef29378f10c342b9a3c84b448e71bb13138442
dmitryfedorov228 <116007725+dmitryfedorov228@users.noreply.github.com>
2023-03-26 16:15:22 +0600

    add files

---
+++ main.js
@@ -0,0 +1,158 @@
+//! Task1
+
+// let a = +prompt("Введите число")
+// let b = +prompt("Введите число")
+// let result = a + b
+
+// switch(result){
+//     case 10: alert(result)
+//     break;
+//     case 20: alert (result)
+//     break;
+//     default: alert("default")
+// }
+
+
+//! Task2
+
+// let month = prompt('Введите название месяца')
+
+// switch(month){
+//     case 'Декабрь':
+//     alert('Зима')
+//     break;
+//     case 'Январь':
+//     alert('Зима')
+//     break;
+//     case 'Февраль':
+//     alert('Зима')
+//     break;
+//     case 'Март':
+//     alert('Весна')
+//     break;
+//     case 'Апрель':
+//     alert('Весна')
+//     break;
+//     case 'Май':
+//     alert('Весна')
+//     break;
+//     case 'Июнь':
+//     alert('Лето')
+//     break;
+//     case 'Июль':
+//     alert('Лето')
+//     break;
+//     case 'Август':
+//     alert('Лето')
+//     break;
+//     case 'Сентябрь':
+//     alert('Осень')
+//     break;
+//     case 'Октябрь':
+//      alert('Осень')
+//     break;
+//     case 'Ноябрь':
+//     alert('Осень')
+//     break;
+        
+// }
+
+//! task3
+
+
+// let years = +prompt('Введите возраст ученика')
+
+// if(years === 6 || years === 7){
+//     alert("Первоклассник")
+// }
+// else if(years === 8 || years ===  9 || years === 10 ) {
+//     alert("ученик из начальной школы")
+// }
+// else if(years === 14 || years === 15 || years === 16) {
+//     alert("ученик из старшей школы")
+// }
+// else{
+//     alert("не ученик")
+// }
+
+//! Task3
+
+// const obj = {
+//     age: 16,
+//     firstName:'Dima',
+//     lastName: 'Fedorov'
+// }
+// console.log(obj ['age']);
+// console.log(obj ['firstName']);
+// console.log(obj ['lastName']);
+
+//! Task4
+
+// const num = [1, 2, 3, 4, 5]
+// console.log(num[2]);
+
+
+//! Task5
+// let name = prompt('Ведите имя')
+// let lastName = prompt('Ведите фамилию')
+// let age = prompt('Ведите возраст')
+
+// let obj = {
+
+// }; 
+// obj.name = name
+// obj.lastName = lastName
+// obj.age = age
+// console.log(obj);
+
+//! Task6
+
+// let a = prompt('Введите значение')
+// const obj = {
+
+// };
+// const arr = [
+
+// ];
+
+
+// if(a === 'Объект'){
+//     obj.result = a
+// }
+// else if(a === "Массив"){
+//     arr.push(a)
+// }
+// console.log(obj);
+// console.log(arr);
+
+//! Extra tasks
+
+//? task1
+
+// const obj = {
+//     value: 5
+// }
+// const arr = [5]
+
+// if(obj ['value'] === arr[0]) {
+//     alert("Значения равны")
+// }    
+
+//? task2
+
+// let a = confirm('2+2 = 4?')
+// const arr = [
+
+// ]
+// const obj = {
+
+// }
+// if(a === true){
+//     arr.push("Верно")
+// }
+// else if(a === false){
+//     obj.isTrue  = " значение неверно" 
+// }
+
+// console.log(arr[0]);
+// console.log(obj);
